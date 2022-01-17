f6e84c2fdcc34428c4f42abe04e90684e5d807fa
naccaratocarolina <naccaratocarolina@gmail.com>
2022-01-17 16:13:14 -0300

    + 10576 - Y2K Accounting Bug

--- Semana-06/10576 - Y2K Accounting Bug.cpp
+++ Semana-06/10576 - Y2K Accounting Bug.cpp
@@ -9,24 +9,25 @@ int main (void) {
 
     for (ano = (1 << 12) - 1; ano >= 0; ano--) {
       int total = 0, aux = 1;
-      for (i=0; i<12; i++)
+      for (i=0; i<12; i++) {
         if ((ano >> i) & 1) total += e;
         else total -= d;
 
-      if (i < 8) {
-        int tmp = 0;
-        for (j=i; i<i+5; j++)
-          if ((ano >> j) & 1) tmp += e;
-          else tmp -= d;
-        if (tmp >= 0) aux = 0;
+        if (i < 8) {
+          int tmp = 0;
+          for (j=i; i<i+5; j++)
+            if ((ano >> j) & 1) tmp += e;
+            else tmp -= d;
+          if (tmp >= 0) aux = 0;
+        }
       }
+      if (aux && excedente_anual < total)
+        excedente_anual = total;
     }
-    if (aux && excedente_anual < total)
-      excedente_anual = total;
+
+    if (excedente_anual < 0) puts("Deficit");
+    else printf("%d\n", excedente_anual);
   }
 
-  if (excedente_anual < 0) puts("Deficit");
-  else printf("%d\n", excedente_anual);
+  return 0;
 }
-
-return 0;
