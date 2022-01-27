e1ce3ff5c5bf7925530eeba735a23954545b2340
naccaratocarolina <naccaratocarolina@gmail.com>
2022-01-27 11:16:30 -0300

    + 10309 - Turn the Lights Off

--- Semana-06/10309 - Turn the Lights Off.cpp
+++ Semana-06/10309 - Turn the Lights Off.cpp
@@ -1,70 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<iostream>
 #include <string.h>
 
-char grid[10][10];
-int tc[10][10];
-int cont;
+using namespace std;
 
-void interruptor (int x, int y) {
-    tc[x][y] = !tc[x][y];
+#define tam 12
+char grid[tam][tam], nome[20];
+int min;
 
-    if (x-1 >= 0) tc[x-1][y] = !tc[x-1][y];
-    if (x+1 < 10) tc[x+1][y] = !tc[x+1][y];
-    if (y-1 >= 0) tc[x][y-1] = !tc[x][y-1];
-    if (y+1 < 10) tc[x][y+1] = !tc[x][y+1];
-}
+int main(void) {
+	int i, j;
 
-void dfs(int x, int y, int passo) {
-    if (y == 10)  x++, y = 0;
+	freopen("input.txt", "r", stdin);
+ 
+	while (scanf("%s", nome)){
+		if(!strcmp(nome, "end")) break;
 
-    if (passo >= cont) return;
+		else if (nome[5] == 'f') min = 0;
 
-    if (x == 0) {
-        dfs(x, y+1, passo);
-        interruptor(x, y);
-        dfs(x, y+1, passo+1);
-        interruptor(x, y);
-    } 
+		else if (nome[5] == 'n') min = 44;
 
-    else if (x < 10) {
-        if(tc[x-1][y] == 1) {
-            interruptor(x, y);
-            dfs(x, y+1, passo+1);
-            interruptor(x, y);
-        } else
-            dfs(x, y+1, passo);
-    } 
-
-    else {
-        int i;
-        for (i = 0; i < 10; i++)
-            if (tc[x-1][i])
-                return;
-        if(passo < cont)  cont = passo;
-    }
-}
-
-int main (void) {
-    char nome[50];
-    int i, j;
-
-    while (gets(nome)) {
-        if (!strcmp(nome, "end"))
-            break;
-
-        for (i = 0; i < 10; i++)
-            gets(grid[i]);
-
-        for (i = 0; i < 10; i++) {
-            for (j = 0; j < 10; j++) {
-                tc[i][j] = grid[i][j] == 'O';
-            }
-        }
-
-        cont = 0xffff;
-        dfs(0, 0, 0);
-        printf("%s %d\n", nome, cont);
-    }
-
-    return 0;
+    for (i = 0; i < 10; i++)
+	  	scanf("%s", grid[i]);
+	}
 }
